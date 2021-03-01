91b2ef6a3b2a1807489ea79a5f3f2a50f2b5ceb5
Leonardo Germán Loza Bonora <leonardogermanlozabonora@gmail.com>
2021-03-01 17:24:00 -0300

    Refactoriza a objetos el capturador

--- public/js/capturador.js
+++ public/js/capturador.js
@@ -1,181 +1,198 @@
 /* ---------- SECCION DE MENSAJES Y JUEGO ----------*/
+class Game {
+    constructor(document) {
+        this.logger = new Logger(1000);
+        this.sender = new Sender("http://localhost:3000/logs");
+        
+        this.messageBackground = document.getElementById('message-background');
 
-const messageBackground = document.getElementById('message-background')
+        this.selectedHeader = selectRandomInArray(document.getElementsByTagName("H5"));
+        document.getElementById('selected-header').innerText = this.selectedHeader.innerText;
 
-const selectedHeader = selectRandomInArray(document.getElementsByTagName("H5"));
-document.getElementById('selected-header').innerText = selectedHeader.innerText
+        //muestra mensaje inicial y comienza captura al tocar el boton
+        this.startButton = document.getElementById('start-button');
+        this.startMessage = document.getElementById('start-message');
+        this.startMessage.style.visibility = "visible";
+        this.startButton.addEventListener('click', this.startGame)
+
+        //desactiva los eventos al tocar el header seleccionado y activa el formulario final
+        this.endMessage = document.getElementById('end-message');
+        this.sendButton = document.getElementById('send-button');
+        this.selectedHeader.addEventListener('click', this.endGame)
+    }
+
+    //los handlers son funciones anonimas para que el this se refiera al objeto
+    startGame = e => {
+        this.messageBackground.style.visibility = "hidden";
+        this.startMessage.style.visibility = "hidden";
+        this.logger.startEventCapture()
+    }
+
+    endGame = e => {
+        this.logger.stopCapture()
+        this.selectedHeader.removeEventListener('click', this.endGame);
+        this.messageBackground.style.visibility = "visible";
+        this.endMessage.style.visibility = "visible";
+        this.sendButton.addEventListener('click', this.submitIfValidate);
+    }
+
+    submitIfValidate = e => {
+        e.preventDefault();
+        if (this.validateEndMessage()) {
+            this.sendButton.disabled = true;
+            this.sender.submitForm(this.transformEndMessage(), this.sendButton);
+        }
+    }
+
+    //transforma los datos del form y logs en un objeto
+    transformEndMessage() {
+        var newObject= {};
+        for(const pair of new FormData(this.endMessage)) {
+            newObject[pair[0]] = pair[1];
+        }
+        newObject.logs = this.logger.logs;
+        console.log(newObject)
+        return newObject;
+    }
+
+    validateEndMessage() {
+        var age = (this.endMessage.value);
+        if (age == "" || age < 1 || age > 100) {
+            alert("Ingrese una edad válida");
+            return false;
+        }
+        return true;
+    }
+}
 
 function selectRandomInArray(array) {
     return array[Math.floor(Math.random() * array.length)]
 }
 
-//muestra mensaje inicial y comienza captura al tocar el boton
-const startButton = document.getElementById('start-button');
-const startMessage = document.getElementById('start-message');
-startMessage.style.visibility = "visible";
-startButton.addEventListener('click', startGame)
-
-function startGame() {
-    messageBackground.style.visibility = "hidden";
-    startMessage.style.visibility = "hidden";
-    startEventCapture()
-}
-
-//desactiva los eventos al tocar el header seleccionado y activa el formulario final
-const endMessage = document.getElementById('end-message');
-const sendButton = document.getElementById('send-button');
-selectedHeader.addEventListener('click', endGame)
-
-function endGame() {
-    stopCapture()
-    selectedHeader.removeEventListener('click', endGame);
-    messageBackground.style.visibility = "visible";
-    endMessage.style.visibility = "visible";
-    sendButton.addEventListener('click', submitIfValidate);
-}
-
-function submitIfValidate(e) {
-    e.preventDefault();
-    if (validateForm(endMessage)) {
-        sendButton.disabled = true;
-        submitForm(createObjectToSend(endMessage));
-    }
-}
-
-function validateForm(form) {
-    var age = form[0].value;
-    if (age == "" || age < 1 || age > 100) {
-        alert("Ingrese una edad válida");
-        return false;
-    }
-    return true;
-}
-
 
 
 /* ---------- SECCION DE CAPTURADOR Y EVENTOS ----------*/
-
-var previousMoveEvent = null;
-var recentMoveEvent = null;
-var recentScrollEvent = null;
-var lastScrollEvent = null;
-var logs = [];
-const miliseconds = 1000;
-const seconds = miliseconds * 0.001;
-var previousSpeed = 0;
-var speed = 0;
-var acceleration = 0;
-
-window.addEventListener('mousemove', mouseMoveUpdate);
-window.addEventListener('scroll', scrollUpdate);
-function startEventCapture() {
-    window.addEventListener('click', captureMouseClick);
-    //intervalo para capturar eventos de movimiento y scrolling
-    setInterval(captureMovementAndScrolling,miliseconds);
-}
-
-function stopCapture() {
-    window.removeEventListener('mousemove', mouseMoveUpdate);
-    window.removeEventListener('click', captureMouseClick);
-    window.removeEventListener('scroll', scrollUpdate);
-}
-
-function mouseMoveUpdate(e) {
-    recentMoveEvent = e;
-}
-
-function scrollUpdate(e) {
-    recentScrollEvent = e;
-}
-
-function captureMouseClick(e) {
-    saveLog('mouse click', positionalData(recentMoveEvent))
-}
-
-function captureMouseMovement() {
-    var actualEvent = recentMoveEvent;
-    var previousEvent = previousMoveEvent;
-    if (previousEvent && actualEvent && (previousEvent != actualEvent)) {
-        var movementX=Math.abs(actualEvent.screenX-previousEvent.screenX);
-        var movementY=Math.abs(actualEvent.screenY-previousEvent.screenY);
-        //hipotenusa de triangulo para saber movimiento
-        var movement=Math.sqrt(movementX*movementX+movementY*movementY);
-        // pixeles / segundos
-        speed=seconds*movement;
-        acceleration=seconds*(speed-previousSpeed);
-
-        const data = positionalData(actualEvent);
-        data.speed = speed;
-        data.acceleration = acceleration;
-        saveLog('mouse movement', data);
+class Logger {
+    constructor(miliseconds) {
+        this.previousMoveEvent = null;
+        this.recentMoveEvent = null;
+        this.recentScrollEvent = null;
+        this.lastScrollEvent = null;
+        this.logs = [];
+        this.seconds = miliseconds * 0.001;
+        this.miliseconds = miliseconds;
+        this.previousSpeed = 0;
+        window.addEventListener('mousemove', this.mouseMoveUpdate);
+        window.addEventListener('scroll', this.scrollUpdate);
     }
-    previousMoveEvent = actualEvent;
-    previousSpeed = speed;
-}
 
-function captureScrolling() {
-    var actualEvent = recentScrollEvent;
-    if (actualEvent && !(actualEvent === lastScrollEvent)) { 
-        saveLog('scrolling', positionalData(recentMoveEvent));
-        lastScrollEvent = actualEvent;
+    startEventCapture() {
+        window.addEventListener('click', this.captureMouseClick);
+        //intervalo para capturar eventos de movimiento y scrolling
+        setInterval(this.captureMovementAndScrolling, this.miliseconds);
     }
-}
 
-function captureMovementAndScrolling() {
-    captureMouseMovement();
-    captureScrolling();
-}
-
-//crea objeto de la data de posicion del mouse
-function positionalData(e) {
-    return {
-        posX: e.clientX,
-        clientY: e.clientY,
-        pageY: e.pageY,
-        outerWidth: window.outerWidth,
-        outerHeight: window.outerHeight,
-        scrollY: window.scrollY,
-        scrollHeight: document.documentElement.scrollHeight
+    stopCapture() {
+        window.removeEventListener('mousemove', this.mouseMoveUpdate);
+        window.removeEventListener('click', this.captureMouseClick);
+        window.removeEventListener('scroll', this.scrollUpdate);
     }
-}
 
-//guarda el Log en el array, poniendo su tipo, hora y data posicional
-function saveLog(newType, newData) {
-    logs.push({
-        type: newType,
-        timestamp: Date.now(),
-        data: newData
-    })
+    captureMouseMovement() {
+        var actualEvent = this.recentMoveEvent;
+        var previousEvent = this.previousMoveEvent;
+        if (previousEvent && actualEvent && (previousEvent != actualEvent)) {
+            var movementX=Math.abs(actualEvent.screenX-previousEvent.screenX);
+            var movementY=Math.abs(actualEvent.screenY-previousEvent.screenY);
+            //hipotenusa de triangulo para saber movimiento
+            var movement=Math.sqrt(movementX*movementX+movementY*movementY);
+            // pixeles / segundos
+            var speed=this.seconds*movement;
+            var acceleration=this.seconds*(speed-this.previousSpeed);
+
+            const data = this.positionalData(actualEvent);
+            data.speed = speed;
+            data.acceleration = acceleration;
+            this.saveLog('mouse movement', data);
+        }
+        this.previousMoveEvent = actualEvent;
+        this.previousSpeed = speed;
+    }
+
+    captureScrolling() {
+        var actualEvent = this.recentScrollEvent;
+        if (actualEvent && !(actualEvent === this.lastScrollEvent)) { 
+            this.saveLog('scrolling', this.positionalData(this.recentMoveEvent));
+            this.lastScrollEvent = actualEvent;
+        }
+    }
+
+    //crea objeto de la data de posicion del mouse
+    positionalData(e) {
+        return {
+            posX: e.clientX,
+            clientY: e.clientY,
+            pageY: e.pageY,
+            outerWidth: window.outerWidth,
+            outerHeight: window.outerHeight,
+            scrollY: window.scrollY,
+            scrollHeight: document.documentElement.scrollHeight
+        }
+    }
+
+    //guarda el Log en el array, poniendo su tipo, hora y data posicional
+    saveLog(newType, newData) {
+        this.logs.push({
+            type: newType,
+            timestamp: Date.now(),
+            data: newData
+        })
+    }
+
+
+    //los handlers son funciones anonimas para que el this se refiera al objeto
+    mouseMoveUpdate = e => {
+        this.recentMoveEvent = e;
+    }
+
+    scrollUpdate = e => {
+        this.recentScrollEvent = e;
+    }
+
+    captureMouseClick = e => {
+        this.saveLog('mouse click', this.positionalData(this.recentMoveEvent))
+    }
+
+    captureMovementAndScrolling = e => {
+        this.captureMouseMovement();
+        this.captureScrolling();
+    }
 }
 
 
 
 /* ---------- SECCION DE ENVIO DE DATOS ----------*/
-
-//transforma los datos del form y logs en un objeto
-function createObjectToSend(form) {
-    var newObject= {};
-    for(const pair of new FormData(form)) {
-        newObject[pair[0]] = pair[1];
+class Sender {
+    constructor(urlToSend) {
+        this.url = urlToSend;
     }
-    newObject.logs = logs;
-    console.log(newObject)
-    return newObject;
+
+    submitForm(object, sendButton) {
+        let xhr = new XMLHttpRequest();
+        xhr.onerror = () => {
+            alert("Ha ocurrido un error en el envío. Intente nuevamente.");
+            sendButton.disabled = false;
+        };
+        xhr.open("POST", this.url, true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.send(JSON.stringify(object));
+        xhr.onload = function() {
+            alert("Sus datos se han enviado correctamente.");
+            document.getElementById('thanks-message').style.visibility = "visible";
+            document.getElementById('end-message').style.visibility = "hidden";
+            console.log(JSON.stringify(xhr.response));
+        };
+    }
 }
 
-function submitForm(e, object) {
-    let xhr = new XMLHttpRequest();
-    xhr.onerror = () => {
-        alert("Ha ocurrido un error en el envío. Intente nuevamente.");
-        sendButton.disabled = false;
-    };
-    xhr.open("POST","https://localhost:3000/logs", true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.send(JSON.stringify(object));
-    xhr.onload = function() {
-        alert("Sus datos se han enviado correctamente.");
-        document.getElementById('thanks-message').style.visibility = "visible";
-        endMessage.style.visibility = "hidden";
-        console.log(JSON.stringify(xhr.response));
-    };
-}
+var game = new Game(document);
